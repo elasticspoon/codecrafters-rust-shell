34d9c30f105e38bdfba8427ab1527074e424eb2f
elasticspoon <mail@yuribocharov.dev>
2025-05-26 19:43:26 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
@@ -9,7 +10,20 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        println!("{}: command not found", input.trim_end());
-        io::stdout().flush().unwrap();
+        handle_command(input);
+    }
+}
+
+fn handle_command(input: String) {
+    let mut parts = input.trim_end().splitn(2, " ");
+    let command = parts.next();
+    match command {
+        Some("exit") => {
+            exit(0);
+        }
+        _ => {
+            println!("{}: command not found", input.trim_end());
+            io::stdout().flush().unwrap();
+        }
     }
 }
