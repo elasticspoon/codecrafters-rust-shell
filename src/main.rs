a29cdaae204745e375839966c5108f99d8844d91
elasticspoon <mail@yuribocharov.dev>
2025-05-26 20:48:06 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::io::{self, Write};
 use std::process::exit;
 
+const VALID_COMMANDS: [&str; 3] = ["echo", "type", "exit"];
+
 fn main() {
     loop {
         print!("$ ");
@@ -18,10 +20,18 @@ fn handle_command(input: String) {
     let mut parts = input.trim_end().splitn(2, " ");
     let command = parts.next();
     match command {
+        Some("type") => {
+            if let Some(type_input) = parts.next() {
+                if VALID_COMMANDS.contains(&type_input) {
+                    println!("{} is a shell builtin", type_input);
+                } else {
+                    println!("{}: not found", type_input);
+                }
+            }
+        }
         Some("echo") => {
             if let Some(echo) = parts.next() {
                 println!("{}", echo);
-                io::stdout().flush().unwrap();
             }
         }
         Some("exit") => {
@@ -29,7 +39,7 @@ fn handle_command(input: String) {
         }
         _ => {
             println!("{}: command not found", input.trim_end());
-            io::stdout().flush().unwrap();
         }
     }
+    io::stdout().flush().unwrap();
 }
