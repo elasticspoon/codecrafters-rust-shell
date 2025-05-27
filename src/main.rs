a78e461d7ad4bba59d35be6677e7816d3d890078
elasticspoon <mail@yuribocharov.dev>
2025-05-27 17:03:23 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{env, process::exit};
 const VALID_COMMANDS: [&str; 3] = ["echo", "type", "exit"];
 
 struct Config {
-    path: String,
+    path: Option<String>,
 }
 
 fn main() {
@@ -16,30 +16,49 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let v = env::var("PATH");
-        handle_command(input);
+        let mut path = None;
+        if let Ok(v) = env::var("PATH") {
+            path = Some(v);
+        }
+        let config = Config { path };
+        handle_command(input, config);
     }
 }
 
-fn handle_command(input: String) {
+fn type_command(command: &str, path: String) {
+    if VALID_COMMANDS.contains(&command) {
+        println!("{} is a shell builtin", command);
+        return;
+    }
+
+    let exec_path = path
+        .split(":")
+        .filter_map(|path| std::fs::read_dir(path).ok())
+        .flat_map(|entries| entries.into_iter().filter_map(|entry| entry.ok()))
+        .find(|value| {
+            if value.file_name() != command {
+                return false;
+            };
+            value.metadata().map_or_else(|_| false, |v| v.is_file())
+        });
+
+    if let Some(path) = exec_path {
+        println!("{} is {}", command, path.path().display());
+    } else {
+        println!("{}: not found", command);
+    }
+}
+
+fn handle_command(input: String, config: Config) {
     let mut parts = input.trim_end().splitn(2, " ");
-    let command = parts.next();
-    match command {
-        Some("type") => {
-            if let Some(type_input) = parts.next() {
-                if VALID_COMMANDS.contains(&type_input) {
-                    println!("{} is a shell builtin", type_input);
-                } else {
-                    println!("{}: not found", type_input);
-                }
-            }
+    match (parts.next(), parts.next()) {
+        (Some("type"), Some(type_input)) => {
+            type_command(type_input, config.path.unwrap_or("".to_string()));
         }
-        Some("echo") => {
-            if let Some(echo) = parts.next() {
-                println!("{}", echo);
-            }
+        (Some("echo"), Some(echo_input)) => {
+            println!("{}", echo_input);
         }
-        Some("exit") => {
+        (Some("exit"), Some(_exit_code)) => {
             exit(0);
         }
         _ => {
