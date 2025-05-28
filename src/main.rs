963cd517333eab6433eb1f86762c1d9bb3c7a2e1
elasticspoon <mail@yuribocharov.dev>
2025-05-28 19:36:09 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -48,15 +48,20 @@ fn type_command(command: &str, path: String) {
     }
 }
 
+fn cd_command(path: &str) {
+    if let Err(e) = std::env::set_current_dir(path) {
+        dbg!(e);
+    }
+}
+
 fn handle_command(input: String, config: Config) {
     let mut parts = input.trim_end().splitn(2, " ");
     match (parts.next(), parts.next()) {
+        (Some("cd"), Some(path)) => {
+            cd_command(path);
+        }
         (Some("pwd"), None) => {
-            if let Ok(path) = std::env::current_dir() {
-                println!("{}", path.to_str().unwrap());
-            } else {
-                panic!("invalid current_dir")
-            }
+            pwd_command();
         }
         (Some("type"), Some(type_input)) => {
             type_command(type_input, config.path.unwrap_or(".".to_string()));
@@ -68,29 +73,41 @@ fn handle_command(input: String, config: Config) {
             exit(0);
         }
         (Some(command), args) => {
-            let output = if let Some(command_args) = args {
-                Command::new(command).args(command_args.split(" ")).output()
-            } else {
-                Command::new(command).output()
-            };
-            match output {
-                Ok(result) => {
-                    stdout()
-                        .write_all(&result.stdout)
-                        .expect("Failed write to stdout.");
-                    stderr()
-                        .write_all(&result.stderr)
-                        .expect("Failed write to stderr.")
-                }
-                Err(e) if e.kind() == ErrorKind::NotFound => {
-                    println!("{}: command not found", command)
-                }
-                Err(e) => panic!("{:?} error when executing command", e),
-            }
+            exec_command(args, command);
         }
         _ => {
             println!("command not found");
         }
     }
     io::stdout().flush().unwrap();
+}
+
+fn pwd_command() {
+    if let Ok(path) = std::env::current_dir() {
+        println!("{}", path.to_str().unwrap());
+    } else {
+        panic!("invalid current_dir")
+    }
+}
+
+fn exec_command(args: Option<&str>, command: &str) {
+    let output = if let Some(command_args) = args {
+        Command::new(command).args(command_args.split(" ")).output()
+    } else {
+        Command::new(command).output()
+    };
+    match output {
+        Ok(result) => {
+            stdout()
+                .write_all(&result.stdout)
+                .expect("Failed write to stdout.");
+            stderr()
+                .write_all(&result.stderr)
+                .expect("Failed write to stderr.")
+        }
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            println!("{}: command not found", command)
+        }
+        Err(e) => panic!("{:?} error when executing command", e),
+    }
 }
